a63fdf0417be644dc7e6879c56c9d8088c1cc84b
Ansari Farah <ziaisme@outlook.com>
2026-05-29 21:43:15 +0530

    Updated Code

--- permutationstr.cpp
+++ permutationstr.cpp
@@ -6,23 +6,23 @@ using namespace std;
 int main() {
 
     string str1 = "ab";
-    string str2 = "eidabooo";
+    string str2 = "eidboaoo";
     string str3 = "";
 
     int i, j;
     for(i = 0; i < str2.length(); i++) {
-        for(j = i; j < str1.length(); j++) {
+        for(j = i; j < str1.length() + i; j++) {
             str3.push_back(str2[j]);
         }
         if(str1 == str3) {
             cout << "True";
             return 0;
         }
-        // reverse(str3.begin(), str3.end());
-        // if(str1 == str3) {
-        //     cout << "True";
-        //     return 0;
-        // }
+        reverse(str3.begin(), str3.end());
+        if(str1 == str3) {
+            cout << "True";
+            return 0;
+        }
 
         while(str3.length() > 0) {
             str3.pop_back();
